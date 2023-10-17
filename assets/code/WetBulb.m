5570a281bb1e32bb9bd186e6fd4ef344e342a87e
Bob Kopp <robert.kopp@rutgers.edu>
2023-10-17 10:31:02 -0400

    update note

--- assets/code/WetBulb.m
+++ assets/code/WetBulb.m
@@ -1,7 +1,8 @@
 # This URL should not be publicly visible, but was used in a 
 # October 2023 note by Cassandra Rogers and Rob Warren
 # regarding the 2016 MATLAB port of HumanIndexMod wet
-# bulb temperature estimation code. 
+# bulb temperature estimation code. This file is here as a 
+# pointer for readers who may have stumbled upon this URL.
 #
 # The correct URL for this port is: https://github.com/bobkopp/WetBulb.m
 
